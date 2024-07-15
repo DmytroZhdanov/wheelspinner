4be60e040af27f9c5e2493908d58d911cc59caab
Dmytro Zhdanov <plumage-bassoon0u@icloud.com>
2024-07-15 18:38:44 +0200

    Add markup and styles

--- page.php
+++ page.php
@@ -12,10 +12,35 @@ Template Post Type: page
   <meta charset="UTF-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
   <title>Document</title>
+  <?php wp_head(); ?>
 </head>
 
 <body>
-  <h1>Hello</h1>
+  <main class="container main_container">
+    <div class="wheel_container">
+      <div class="wheel">
+        <button type="button" class="push_btn">Push</button>
+      </div>
+    </div>
+
+    <div class="options_container">
+      <ul class="options_list">
+        <li class="option_item">Option 1</li>
+        <li class="option_item">Option 2</li>
+        <li class="option_item">Option 3</li>
+        <li class="option_item">Option 4</li>
+        <li class="option_item">Option 5</li>
+      </ul>
+
+      <div class="add_option">
+        <input type="text" class="new_option" placeholder="New option">
+
+        <button type="button" aria-label="Add new option" class="add_option_btn">+</button>
+      </div>
+    </div>
+  </main>
 </body>
+
+<?php wp_footer(); ?>
 
 </html>
