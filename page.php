8677eee85758eefd2aa12ba23ff47dbf3b6ececf
Dmytro Zhdanov <plumage-bassoon0u@icloud.com>
2024-07-18 18:10:47 +0200

    Refactor page.php

--- page.php
+++ page.php
@@ -3,6 +3,10 @@
 Template Name: Wheelspinner
 Template Post Type: page
 */
+
+$options = [
+  "Option 1", "Option 2", "Option 3", "Option 4", "Option 5"
+]
 ?>
 
 <!DOCTYPE html>
@@ -22,11 +26,9 @@ Template Post Type: page
     <div class="main_container">
       <div class="wheel_container">
         <div class="wheel">
-          <span id="label_1" class="sector_label" style="transform: rotate(-126deg)">Option 1</span>
-          <span id="label_2" class="sector_label" style="transform: rotate(-54deg)">Option 2</span>
-          <span id="label_3" class="sector_label" style="transform: rotate(18deg)">Option 3</span>
-          <span id="label_4" class="sector_label" style="transform: rotate(90deg)">Option 4</span>
-          <span id="label_5" class="sector_label" style="transform: rotate(162deg)">Option 5</span>
+          <?php foreach ($options as $index => $option) : ?>
+            <span id="label_<?php echo $index + 1; ?>" class="sector_label" style="transform: rotate(<?php echo (-90 - (180 / count($options)) + (360 / count($options) * $index)) ?>deg)"><?php echo $option; ?></span>
+          <?php endforeach; ?>
         </div>
 
         <button type="button" class="push_btn">Push</button>
@@ -34,45 +36,15 @@ Template Post Type: page
 
       <div class="options_container">
         <ul class="options_list">
-          <li class="option_item" id="option_item_1">
-            <input type="text" id="option_input_1" class="option_item_input" value="Option 1" />
+          <?php foreach ($options as $index => $option) : ?>
+            <li class="option_item" id="option_item_<?php echo $index + 1; ?>">
+              <input type="text" id="option_input_<?php echo $index + 1; ?>" class="option_item_input" value="<?php echo $option; ?>" />
 
-            <button type="button" id="remove_option_btn_1" class="remove_icon_btn">
-              <img id="remove_option_icon_1" src="<?php echo get_template_directory_uri(); ?>/assets/icons/remove_icon.svg" alt="delete" class="remove_icon">
-            </button>
-          </li>
-
-          <li class="option_item" id="option_item_2">
-            <input type="text" id="option_input_2" class="option_item_input" value="Option 2" />
-
-            <button type="button" id="remove_option_btn_2" class="remove_icon_btn">
-              <img id="remove_option_icon_2" src="<?php echo get_template_directory_uri(); ?>/assets/icons/remove_icon.svg" alt="delete" class="remove_icon">
-            </button>
-          </li>
-
-          <li class="option_item" id="option_item_3">
-            <input type="text" id="option_input_3" class="option_item_input" value="Option 3" />
-
-            <button type="button" id="remove_option_btn_3" class="remove_icon_btn">
-              <img id="remove_option_icon_3" src="<?php echo get_template_directory_uri(); ?>/assets/icons/remove_icon.svg" alt="delete" class="remove_icon">
-            </button>
-          </li>
-
-          <li class="option_item" id="option_item_4">
-            <input type="text" id="option_input_4" class="option_item_input" value="Option 4" />
-
-            <button type="button" id="remove_option_btn_4" class="remove_icon_btn">
-              <img id="remove_option_icon_4" src="<?php echo get_template_directory_uri(); ?>/assets/icons/remove_icon.svg" alt="delete" class="remove_icon">
-            </button>
-          </li>
-
-          <li class="option_item" id="option_item_5">
-            <input type="text" id="option_input_5" class="option_item_input" value="Option 5" />
-
-            <button type="button" id="remove_option_btn_5" class="remove_icon_btn">
-              <img id="remove_option_icon_5" src="<?php echo get_template_directory_uri(); ?>/assets/icons/remove_icon.svg" alt="delete" class="remove_icon">
-            </button>
-          </li>
+              <button type="button" id="remove_option_btn_<?php echo $index + 1; ?>" class="remove_icon_btn">
+                <img id="remove_option_icon_<?php echo $index + 1; ?>" src="<?php echo get_template_directory_uri(); ?>/assets/icons/remove_icon.svg" alt="delete" class="remove_icon">
+              </button>
+            </li>
+          <?php endforeach; ?>
         </ul>
 
         <div class="add_option">
